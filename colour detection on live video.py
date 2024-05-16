775711dec4a2b5bfc349e06240ed8699c80c2c3e
pwardion <berkayilikoba26@gmail.com>
2024-05-16 18:36:36 +0300

    Create colour detection on live video.py

---
+++ colour detection on live video.py
@@ -0,0 +1,25 @@
+#colour detection on live video
+
+import cv2 as cv
+cap = cv.VideoCapture(0)
+
+while cap.isOpened():
+    ret,frame = cap.read()
+    low = (160, 50, 50)
+    up = (180, 255, 255)
+
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, low, up)  # inrange maskeleme için kullanılır
+
+    res = cv.bitwise_and(frame, frame, mask=mask)
+
+    cv.imshow("original", frame)
+
+    cv.imshow("masked", mask)
+
+    cv.imshow("result", res)
+    cv.waitKey(1)
+
+cap.release()
+cv.destroyAllWindows()
+
